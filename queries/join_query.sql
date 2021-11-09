2b3e44ba454b94c186385b1918718d86566f2e0e
Jori <jvalkenhoff@outlook.com>
2021-11-09 22:29:31 +0100

    adding the longest query i ever wrote

--- queries/join_query.sql
+++ queries/join_query.sql
@@ -1,11 +1,21 @@
+WITH ceiled_salaries AS (
+	SELECT CEILING(salary / 10000) * 10000 AS ceiled_salary 
+	FROM employees
+)
 SELECT 
-	emp.first_name AS first_name ,
+	emp.first_name AS first_name,
 	emp.last_name AS last_name,
-	emp.salary AS salary,
+	CEILING(emp.salary / 10000) * 10000 AS ceiled_salary,
 	jobs.job_title AS job_title,
 	man.first_name || ' ' || man.last_name AS manager_full_name,
 	departments.department_name AS department_name,
-	locations.city || ', ' || countries.country_name || ', ' || regions.region_name AS current_location
+	locations.city || ', ' || countries.country_name || ', ' || regions.region_name AS current_location,
+	(
+		SELECT COUNT(ceiled_salary)
+		FROM ceiled_salaries
+		GROUP BY ceiled_salary
+		HAVING ceiled_salary = (CEILING(emp.salary / 10000) * 10000)
+	) AS salary_count
 FROM 
 	employees emp
 	LEFT JOIN employees man ON emp.manager_id = man.employee_id
@@ -15,4 +25,4 @@ FROM
 	INNER JOIN countries ON countries.country_id = locations.country_id
 	INNER JOIN regions ON regions.region_id = countries.region_id
 ORDER BY
-	salary DESC
+	ceiled_salary DESC;
