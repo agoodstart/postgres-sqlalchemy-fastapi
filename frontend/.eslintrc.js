84784b1fdbd563290ccea49881c881cf3ca8f2f1
Jori <jvalkenhoff@outlook.com>
2021-11-16 11:06:36 +0100

    adding linter

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        'plugin:vue/essential',
+        'plugin:vue/strongly-recommended',
+        'plugin:vue/recommended'
+    ],
+    rules: {
+        'vue/no-multiple-template-root': 'off'
+    }
+}
