263f532199158baa6bacec6955a37f5d75e7bdaa
Jori <jvalkenhoff@outlook.com>
2021-11-18 15:41:36 +0100

    wiring up authentication

---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,39 @@
+<template>
+
+</template>
+
+<script>
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import * as yup from 'yup'
+
+export default {
+    name: 'Signup',
+    components: {
+        Form,
+        Field,
+        ErrorMessage
+    },
+    data() {
+        const schema = yup.object().shape({
+            email: yup
+                .string()
+                .required("Email is required")
+                .email("EMail is invalid")
+                .max(50, "Must have a maximum of 50 characters"),
+            password: yup
+                .string()
+                .required("Password is required!")
+                .min(6, "Must be at least 6 characters")
+                .max(20, "Must have a maximum of 20 characters")
+        });
+
+        return {
+            successful: false,
+            loading: false,
+            message: "",
+            schema,
+        };
+    },
+    
+}
+</script>
